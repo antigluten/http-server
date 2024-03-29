b1d626ae6196cfd461c5bc32291990b0c67872bb
Vladimir Gusev <antigluten@icloud.com>
2024-03-29 22:58:41 +0300

    Try respond with 200

--- server.c
+++ server.c
@@ -47,8 +47,11 @@ int main() {
     printf("Waiting for a client to connect...\n");
     client_addr_len = sizeof(client_addr);
 
-    accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
     printf("Client connected\n");
+
+    const char *response = "HTTP/1.1 200 OK\r\n\r\n";
+    send(client_fd, response, strlen(response), 0);
 
     close(server_fd);
 
